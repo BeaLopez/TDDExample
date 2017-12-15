7286fb936aa81f1ee722b9f6de4a1200a24cd55e
Beatriz López Morales <b.lopez.morales@accenture.com>
2017-12-15 14:50:30 +0100

    Test UI

--- app/src/main/java/com/blopez/tddexample/MainActivity.kt
+++ app/src/main/java/com/blopez/tddexample/MainActivity.kt
@@ -2,11 +2,22 @@ package com.blopez.tddexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.blopez.tddexample.model.Orientation
+import com.blopez.tddexample.model.Rover
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            var rover = Rover()
+            rover.applyCommand(editText.text.toString())
+            if(rover.direction.equals(Orientation.EAST)){
+                textView2.text = "OKKKK"
+            }
+        }
+
     }
 }
